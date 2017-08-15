bda4846b37f5fe95bd7a0adc951be4419df072ae
York Tsai <v-yotsai@microsoft.com>
2017-08-16 01:40:58 +0800

    init

--- FB_ChatBot/Dialogs/RootDialog.cs
+++ FB_ChatBot/Dialogs/RootDialog.cs
@@ -23,7 +23,11 @@ namespace FB_ChatBot.Dialogs
             int length = (activity.Text ?? string.Empty).Length;
 
             // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+            //await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+            if(activity.Text == "你好")
+                await context.PostAsync($"Hello，你可以問我關於領袖營的問題喔");
+            else
+                await context.PostAsync($"工程師還在開發中...");
 
             context.Wait(MessageReceivedAsync);
         }
